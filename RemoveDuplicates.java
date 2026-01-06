a9e6893679d928ea0d76159ecc32ba75f0c6cbe5
ishikaupadhyay <ishikaupadhyay1308@gmail.com>
2026-01-06 14:17:23 +0530

    Left Rotate array by one -Method I

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,61 +1,68 @@
-import java.util.HashSet;
+// for method 1:
+// import java.util.HashSet;
+
 import java.util.Arrays;
 
 class Solution {
-//    // public int removeDuplicates(int[] nums)
-//      { //METHID-1(BRUTE FORCE )TC-O(N),SC-O(N)
-//      HashSet<Integer> seen=new HashSet<>();
-     
-//      int index=0;//starts frm this index
 
-//      for(int num:nums)
-//      {
-//         if(!seen.contains(num))
-//         {
-//             seen.add(num);
+    // METHOD-1 (BRUTE FORCE)
+    // TC - O(N), SC - O(N)
+    /*
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
 
-//             nums[index]=num;//jitne bhi no(unique) num mai aayenga...utna index mai aajaega (mttlb  no.of uniq elements)
-//             index++;//aage badhte chlo
-//         }
-//      }return index;
-//     }
+        int index = 0; // starts from this index
 
-
-//METHOD - 2(OPTIMAL),O(n)&O(1)
-   public int removeDuplicates(int[] nums)
-{ Arrays.sort(nums);
-    int n=nums.length;
-
-    if (n==0)return 0;
-
-    //MAIN CONDITION:
-    int i=0; //strting frm index 1 
-//j=nearby element of i 
-//its a sorted array ,so duplicate elements if exist stays nearby only!
-for(int j=1;j<n;j++){
-    if (nums[j]!=nums[i])
-    i++;
-    nums[i]=nums[j];//if ele=uniq , bring it forward!
-}
-return i+1;//index ('kitne uniq el nkle')
-}
-
-
-
-    public static void main(String[]args){
-        int []nums={1,2,36,1,2,5,1,36,15};
-
-        Solution sol=new Solution();
-        int k =sol.removeDuplicates(nums);
-
-        System.out.println("NO OF UNIQUE elements ARE :"+k);
-
-        //printing new array :
-        System.out.println("NEW ARRAY : ");
-
-        for(int i=0;i<k;i++){
-            System.out.print(nums[i]+" ");
+        for (int num : nums) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                nums[index] = num; // unique elements moved forward
+                index++;
+            }
         }
-    
+        return index;
+    }
+    */
+
+    // METHOD-2 (OPTIMAL)
+    // TC - O(N log N) due to sorting
+    // SC - O(1)
+    public int removeDuplicates(int[] nums) {
+
+        Arrays.sort(nums); // sorting the array
+        int n = nums.length;
+
+        if (n == 0) return 0;
+
+        int i = 0; // index for unique elements
+
+        // j is the nearby element of i
+        // since array is sorted, duplicates stay adjacent
+        for (int j = 1; j < n; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j]; // bring unique element forward
+            }
+        }
+
+        return i + 1; // number of unique elements
+    }
+}
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 36, 1, 2, 5, 1, 36, 15};
+
+        Solution sol = new Solution();
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("NO OF UNIQUE elements ARE : " + k);
+
+        // printing new array
+        System.out.println("NEW ARRAY : ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
---
+++ leftrotateMethodI.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotateArrayByOne(int[] nums) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            newArr[i - 1] = nums[i];
+        }
+        newArr[n - 1] = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = newArr[i];
+        }
+    }
+}
+public class leftrotateMethodI {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6, 7};
+        Solution sol = new Solution();
+        sol.rotateArrayByOne(nums);
+
+        for (int x : nums) {
+            System.out.print(x + " ");
+        }
+    }
+}
