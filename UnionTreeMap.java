53d75a5d86021a5988a75a2746b685b0d5e6324c
ishikaupadhyay <142908140+ishikaupadhyay@users.noreply.github.com>
2026-01-08 14:22:52 +0530

    Document time and space complexity in UnionTreeMap
    Added time and space complexity comments for UnionTreeMap.

--- UnionTreeMap.java
+++ UnionTreeMap.java
@@ -39,4 +39,8 @@ public class UnionTreeMap {
         }
 
     }
-}
+}//Time Compleixty : O( (m+n)log(m+n) ) . Inserting a key in map takes logN times, where N is no of elements in map. At max map can store m+n elements {when there are no common elements and elements in arr,arr2 are distntict}. So Inserting m+n th element takes log(m+n) time. Upon approximation across insertion of all elements in worst it would take O((m+n)log(m+n) time.
+
+//Using HashMap also takes the same time, On average insertion in unordered_map takes O(1) time but sorting the union vector takes O((m+n)log(m+n))  time. Because at max union vector can have m+n elements.
+
+//Space Complexity : O(m+n) {If Space of Union ArrayList is considered} ,O(1) {If Space of union ArrayList is not considered}
