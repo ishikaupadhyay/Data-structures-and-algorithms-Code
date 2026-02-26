b4a10cb7c054c97168d071b3d747d7900cbaf9af
ishikaupadhyay <ishikaupadhyay1308@gmail.com>
2026-02-26 16:20:04 +0530

    FIND NEXT GREATER ELEMENT

--- nextGreaterElement.java
+++ nextGreaterElement.java
@@ -1,16 +1,33 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class nextGreaterElement {
-    public int FindNextGreaterElement(int arr[]) {
+    public int[] FindNextGreaterElement(int arr[]) {
 
-        for(int i=0;i<arr.length;i++){
-            int count=0;
+        // creating a satck inw which elemment pushing(appropraiet one) will take place
+        Stack<Integer> st = new Stack<>();
 
-            for(int j=i;j<arr.length;j++){
-                if(j>i)
+        int[] resultarr = new int[arr.length];// to store the result arr
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() <= arr[i]) {
+                st.pop();
             }
+            if (st.isEmpty()) {
+                resultarr[i] = -1;
+            } else {
+                resultarr[i] = st.peek();// jo sbse top pr hai wahi ans hai!
+            }
+            st.push(arr[i]);
         }
+        return resultarr;
     }
 
     public static void main(String[] args) {
         int arr[] = { 6, 8, 0, 1, 3 };
+        nextGreaterElement result = new nextGreaterElement();
+        int ans[] = result.FindNextGreaterElement(arr);
+        System.out.println(Arrays.toString(ans));
     }
 }
