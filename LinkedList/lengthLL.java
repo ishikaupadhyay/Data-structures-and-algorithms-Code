54fb3a276ca37588e76ce3de4beb77bba816507b
ishikaupadhyay <ishikaupadhyay1308@gmail.com>
2026-03-09 16:18:24 +0530

    LENGTH OF LINKED LIST

--- LinkedList/lengthLL.java
+++ LinkedList/lengthLL.java
@@ -1,5 +1,5 @@
-public  class lengthLL {
-     static class Node {
+public class lengthLL {
+    static class Node {
         int data;
         Node next;
 
@@ -10,16 +10,29 @@ public  class lengthLL {
         }
     }
 
-    static class Solution{
-        public int FindLengthLL(Node head){
-            Node temp=head;
-            int count=0
+    static class Solution {
+        public static int FindLengthLL(Node head) {
+            Node temp = head;
+            int count = 0;
 
+            while (temp != null) {
+                count++;
+                temp = temp.next;// aage badhte chalooooooo
+            }
 
+            return count;
         }
     }
 
     public static void main(String[] args) {
+
+        Node head = new Node(7);
+        head.next = new Node(3);
+        head.next.next = new Node(9);
+        head.next.next.next = new Node(2);
+
+        Solution sol = new Solution();
+        System.out.println("LENGTH OF THE LINKED LIST : " + sol.FindLengthLL(head));
 
     }
 }
